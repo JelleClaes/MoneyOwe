40b5a68e3a21f0624926d8f1b785d542be9b9662
JelleClaes <jelle.claes7@gmail.com>
2014-05-05 15:35:00 +0200

    test

--- js/main.js
+++ js/main.js
@@ -14,31 +14,23 @@ function dbErrorHandler(err){
 
 function phoneReady(){
     doLog("phoneReady");
-    //First, open our db
-    
-    dbShell = window.openDatabase("SimpleNotes", 2, "SimpleNotes", 1000000);
+    dbShell = window.openDatabase("Contacts", 1, "Contacts", 1000000);
     doLog("db was opened");
-    //run transaction to create initial tables
     dbShell.transaction(setupTable,dbErrorHandler,getEntries);
     doLog("ran setup");
 }
 
-//I just create our initial table - all one of em
 function setupTable(tx){
     doLog("before execute sql...");
-    tx.executeSql("CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY,title,body,updated)");
+    tx.executeSql("CREATE TABLE IF NOT EXISTS schulden(id INTEGER PRIMARY KEY,name,amount,status,updated)");
     doLog("after execute sql...");
 }   
 
-//I handle getting entries from the db
 function getEntries() {
-    
-    //doLog("get entries");
     dbShell.transaction(function(tx) {
-        tx.executeSql("select id, title, body, updated from notes order by updated desc",[],renderEntries,dbErrorHandler);
+        tx.executeSql("select id, name, amount,status, updated from schulden order by updated name",[],renderEntries,dbErrorHandler);
     }, dbErrorHandler);
 }
-
     
 function renderEntries(tx,results){
     doLog("render entries");
@@ -47,7 +39,7 @@ function renderEntries(tx,results){
     } else {
        var s = "";
        for(var i=0; i<results.rows.length; i++) {
-         s += "<li><a href='edit.html?id="+results.rows.item(i).id + "'>" + results.rows.item(i).title + "</a></li>";   
+         s += "<li><a href='edit.html?id="+results.rows.item(i).id + "'>" + results.rows.item(i).name + "</a></li>";   
        }
        $("#noteTitleList").html(s);
        $("#noteTitleList").listview("refresh");
@@ -55,18 +47,16 @@ function renderEntries(tx,results){
 }
 
 function saveNote(note, cb) {
-    //Sometimes you may want to jot down something quickly....
     if(note.title == "") note.title = "[No Title]";
     dbShell.transaction(function(tx) {
-        if(note.id == "") tx.executeSql("insert into notes(title,body,updated) values(?,?,?)",[note.title,note.body, new Date()]);
-        else tx.executeSql("update notes set title=?, body=?, updated=? where id=?",[note.title,note.body, new Date(), note.id]);
+        if(note.id == "") tx.executeSql("insert into notes(name,amount,updated) values(?,?,?)",[note.title,note.body, new Date()]);
+        else tx.executeSql("update notes set name=?, amount=?,status=?, updated=? where id=?",[note.title,note.body,note.body, new Date(), note.id]);
     }, dbErrorHandler,cb);
 }
 
 function init(){
     document.addEventListener("deviceready", phoneReady, false);
     
-    //handle form submission of a new/old note
     $("#editNoteForm").live("submit",function(e) {
         var data = {title:$("#noteTitle").val(), 
                     body:$("#noteBody").val(),
@@ -78,19 +68,15 @@ function init(){
         e.preventDefault();
     });
 
-    //will run after initial show - handles regetting the list
     $("#homePage").live("pageshow", function() {
         getEntries(); 
     });
 
-    //edit page logic needs to know to get old record (possible)
     $("#editPage").live("pageshow", function() {
-        //get the location - it is a hash - got to be a better way
-        var loc = window.location.hash;
+        var loc = var loc = $(this).data("url");
         if(loc.indexOf("?") >= 0) {
             var qs = loc.substr(loc.indexOf("?")+1,loc.length);
             var noteId = qs.split("=")[1];
-            //load the values
             $("#editFormSubmitButton").attr("disabled","disabled"); 
             dbShell.transaction(
                 function(tx) {
@@ -100,8 +86,7 @@ function init(){
                         $("#noteBody").val(results.rows.item(0).body);
                         $("#editFormSubmitButton").removeAttr("disabled");   
                     });
-                }, dbErrorHandler);
-            
+                }, dbErrorHandler);            
         } else {
          $("#editFormSubmitButton").removeAttr("disabled");   
         }
